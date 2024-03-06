97ef455c091d1fd007669c84a4231e6d542484b6
ppannawitt <106550775+ppannawitt@users.noreply.github.com>
2024-03-06 06:36:42 +0000

    submit

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 num = input('Enter a number (decimal only): ')
 # type your code here
-ind = -1;
-for i in range(len(num)):
-  if (num[i]=='.'):
-    ind = i
-    break
-dp = len(num) - ind -1
+index = num.find('.') 
+remaining = num[index+1:]
+dp = len(remaining)
 
 
 # do not change any code beyond this point

61f400aad319a10485b0521116d7b88e640c7222
ppannawitt <106550775+ppannawitt@users.noreply.github.com>
2024-03-06 02:40:32 +0000

    submit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 num = input('Enter a number (decimal only): ')
 # type your code here
-
+ind = -1;
+for i in range(len(num)):
+  if (num[i]=='.'):
+    ind = i
+    break
+dp = len(num) - ind -1
 
 
 # do not change any code beyond this point
